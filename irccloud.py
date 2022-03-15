79a890d5cf62f9bc2b9604a6d7b93b8bde3b0182
rockages <81442955+rockages@users.noreply.github.com>
2022-03-15 20:50:06 +0700

    Update irccloud.py

--- irccloud.py
+++ irccloud.py
@@ -84,8 +84,8 @@ class irccloud:
         }
         while True:
             r = requests.post(stream_url, headers = headers)
-            #if self.debugging:	
-                #self.log.debug(r.json())
+            if self.debugging:	
+                self.log.debug(r.json())
             if r.status_code == 200:
                 irccloud.KeepAliveToken = "KA_ALIVE"
                 break
